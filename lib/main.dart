1adb20ecf52fe1df56ad0c2e9f0039dad154e6b2
msolimann2cs <msolimann2cs@gmail.com>
2022-06-20 18:34:58 +0200

    Not working yet. Audioplayers plugin has a bug. Audioplayer plugin cannot run with sound null safety.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
+//import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayer/audioplayer.dart';
 
-void main() {
-  runApp();
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  AudioPlayer audioPlugin = AudioPlayer();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            child: Center(
+              child: TextButton(
+                onPressed: () {
+                  //final player = AudioCache();
+                  audioPlugin.play('note1.wav');
+                },
+                child: Text('Click Me'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
